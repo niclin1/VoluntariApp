ef774ac7d3b96086edcfa1174c6fd2789d4f902c
Lucas Sousa Alves <91626518+LucasSousaAl@users.noreply.github.com>
2026-03-17 18:29:29 +0000

    removed index.js

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from 'infra/database';
+import database from "infra/database";
 // lembra de testar o comando compose down sem e com o -v para ver a persistencia dos dados
 
 async function status(req, res) {
@@ -6,20 +6,21 @@ async function status(req, res) {
   const databaseHealth = await database.query("SELECT version();");
   const databaseMaxConnections = await database.query("SHOW max_connections;");
   const databaseConnectionsUsed = await database.query({
-    text:"SELECT count(*)::int AS connections FROM pg_stat_activity WHERE datname = $1 AND backend_type = 'client backend';",
-    values: [process.env.POSTGRES_DB]
+    text: "SELECT count(*)::int AS connections FROM pg_stat_activity WHERE datname = $1 AND backend_type = 'client backend';",
+    values: [process.env.POSTGRES_DB],
   });
-  res.status(200).json({ 
+
+  res.status(200).json({
     updated_at: updatedAt,
     dependencies: {
       database: {
         version: databaseHealth?.[0]?.version || null,
-        max_connections: Number(databaseMaxConnections?.[0]?.max_connections) || null,
-        connections_used: databaseConnectionsUsed?.[0]?.connections || null
-      }
-    }
-   });
+        max_connections:
+          Number(databaseMaxConnections?.[0]?.max_connections) || null,
+        connections_used: databaseConnectionsUsed?.[0]?.connections || null,
+      },
+    },
+  });
 }
-
 
 export default status;
